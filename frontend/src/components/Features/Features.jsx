fd742bf224ed52d88c9eeabc6a1a32f94fb7ebc0
emelinew1 <emeline.wulstecke@gmail.com>
2024-03-27 15:51:46 +0100

    Features created

--- frontend/src/components/Features/Features.jsx
+++ frontend/src/components/Features/Features.jsx
@@ -1,8 +1,22 @@
 import './features.css';
 import React from 'react';
+import featuresData from '../../data/featuresData.json';
+
 
 const Features = () => {
-    
+    return (
+        <section className='features'>
+            <h2 className='sr-only'>Features</h2>
+            {featuresData.map((feature, index) => (
+                <figure className="feature-item" key={index}>
+                    <img src={feature.icon} alt="Feature Icon" className="feature-icon" />
+                    <h3 className="feature-item-title">{feature.title}</h3>
+                    <p>{feature.description}</p>
+                </figure>
+            ))}
+        </section>
+    )
+
 }
 
 export default Features;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './home.css';
 import Banner from '../../components/Banner/Banner';
+import Features from '../../components/Features/Features';
 
 const Home = () => {
     return (
         <main className="main">
             <Banner />
+            <Features />
         </main>
     )
 }
