c6bbca2ddf4151ad6cef40093d34b00e4b8ca4ba
emelinew1 <emeline.wulstecke@gmail.com>
2024-03-27 15:17:37 +0100

    Banner created

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './home.css';
+import Banner from '../../components/Banner/Banner';
 
 const Home = () => {
-    
+    return (
+        <main className="main">
+            <Banner />
+        </main>
+    )
 }
 
 export default Home;
