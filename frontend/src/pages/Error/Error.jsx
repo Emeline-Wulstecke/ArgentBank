dd0281ac900d0f6b9049e1899dca79db7ad012ca
emelinew1 <emeline.wulstecke@gmail.com>
2024-03-28 20:44:55 +0100

    login + error pages created

--- frontend/src/pages/Error/Error.jsx
+++ frontend/src/pages/Error/Error.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './error.css';
+import exit from './../../assets/images/exit.webp';
 
 const Error = () => {
-    
+    return (
+        <main className='main-error'>
+            <section className="error">
+                <h1 className='title-error'>404</h1>
+                <p className="text-error">Page not found</p>
+                <NavLink to="/" className="link-error">
+                    <img src={exit} alt="exit" />
+                    <p>Back to home Page</p>
+                </NavLink>
+            </section>
+        </main>
+    );
 }
 
 export default Error;
