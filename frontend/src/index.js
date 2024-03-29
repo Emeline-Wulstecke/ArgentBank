207703cbee5ec24bd998126e078c474e5a6da219
emelinew1 <emeline.wulstecke@gmail.com>
2024-03-29 15:54:39 +0100

    installation react redux, add files & routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; 
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+//Redux 
+
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers/store.js';
+
+
+const store= configureStore({
+  reducer: rootReducer,
+  devTools: true,
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
+    <Provider store={store}>
     <App />
-  </React.StrictMode>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ frontend/src/reducers/users.reducer.js
@@ -0,0 +1,15 @@
+
+const initialState = {
+    currentUser: null,
+    token: null,
+    error: null,
+    loading: false,
+
+}
+
+export default function users(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
