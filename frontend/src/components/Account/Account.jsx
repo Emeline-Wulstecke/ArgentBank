56f415c0fc209f0f86786e005ee91a79a3b8dd37
emelinew1 <emeline.wulstecke@gmail.com>
2024-04-12 15:07:15 +0200

    idea swagger phase two added

--- frontend/src/components/Account/Account.jsx
+++ frontend/src/components/Account/Account.jsx
@@ -1,17 +1,25 @@
 import './account.css';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
-  const Account = ({ type, number, balance, balanceDescription}) => {
+const Account = ({ type, number, balance, balanceDescription }) => {
+    const navigate = useNavigate();
+
+    const redirectToErrorPage = () => {
+        navigate('/error'); // Redirection vers la page d'erreur
+    };
+
+
     return (
         <section id="account">
-        <header>
-            <h3>{type} - {number}</h3>
-            <p className='amount'>{balance}</p>
-            <p>{balanceDescription}</p>
-        </header>
-            <button>View transactions</button>
-    </section>
+            <header>
+                <h3>{type} - {number}</h3>
+                <p className='amount'>{balance}</p>
+                <p>{balanceDescription}</p>
+            </header>
+            <button onClick={redirectToErrorPage}>View transactions</button>
+        </section>
     );
 };
 
